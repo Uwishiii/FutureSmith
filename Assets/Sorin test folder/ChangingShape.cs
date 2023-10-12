9f582172c991a318b8e35f15b96e5f8b8e087fa8
Sor1nel <sofiansorin1003@gmail.com>
2023-10-12 13:25:40 +0200

    Shape change for cube/sphere obj
    You can now hit heated cubes/spheres with Hammer to change their shape. (the cube/sphere first needs to stay in the furnace until it gets the "done" tag)

---
+++ Assets/Sorin test folder/ChangingShape.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingShape : MonoBehaviour
+{
+    private int hits = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Hammer") && hits < 3 && gameObject.CompareTag("done"))
+        {
+            
+
+            Transform cubeTransform = transform;
+
+           
+            if (hits == 0)
+            {
+                cubeTransform.localScale = new Vector3(0.7f, 0.4f, 0.7f);
+            }
+            else if (hits == 1)
+            {
+                cubeTransform.localScale = new Vector3(0.8f, 0.3f, 0.8f);
+            }
+            else if (hits == 2)
+            {
+                cubeTransform.localScale = new Vector3(0.8f, 0.1f, 0.8f);
+            }
+
+            hits++; 
+        }
+    }
+}
