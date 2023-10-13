ebddde311b418f6e9997249480a148d93a5db03b
Sor1nel <sofiansorin1003@gmail.com>
2023-10-13 18:25:22 +0200

    Hologram On/Off for furnace Lever

--- Assets/Sorin test folder/Scripts/LeverControl.cs
+++ Assets/Sorin test folder/Scripts/LeverControl.cs
@@ -7,6 +7,17 @@ public class LeverControl : MonoBehaviour
     public HingeJoint leverHinge;
     public float onAngle = 45.0f; 
     public bool isOn = false;
+    public GameObject targetObjectON;
+    public GameObject targetObjectOFF;
+
+
+
+    void Start()
+    {
+        targetObjectON.SetActive(false);
+        targetObjectOFF.SetActive(true);
+    }
+
 
     void Update()
     {
@@ -16,13 +27,17 @@ public class LeverControl : MonoBehaviour
         {
             isOn = true;
             Debug.Log("is ON");
+            targetObjectON.SetActive(true);
+            targetObjectOFF.SetActive(false);
             //remember to start the heating animation
-            
+
         }
         else if (currentAngle < onAngle && isOn)
         {
             isOn = false;
             Debug.Log("is OFF");
+            targetObjectON.SetActive(false);
+            targetObjectOFF.SetActive(true);
             //remember to stop the heating animation
 
 
